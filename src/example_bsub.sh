3bd1583a43e64e90159e7a30333e1e15b420f06a
Kent Riemondy <kent.riemondy@gmail.com>
2018-01-29 12:05:09 -0700

    use 30Gb

--- src/example_bsub.sh
+++ src/example_bsub.sh
@@ -2,7 +2,7 @@
 #BSUB -J rnastruct 
 #BSUB -o rnastruct_%J.out
 #BSUB -e rnastruct_%J.err
-#BSUB -R "select[mem>10] rusage[mem=10] span[hosts=1]" 
+#BSUB -R "select[mem>30] rusage[mem=30] span[hosts=1]" 
 #BSUB -q normal
 #BSUB -n 12
 
@@ -16,12 +16,13 @@ module load gcc
 module load samtools
 module load python3
 
-BAM="$HOME/Projects/rnastruct/tests/data/med.bam"
+BAM="$HOME/Projects/rnastruct/tests/data/large.bam"
 FA="$HOME/Projects/shared_dbases/genomes/GRCh38.primary_assembly.genome.fa"
-OUTDIR="$HOME/Projects/rnastruct/data/mismatches/medium/"
+OUTDIR="$HOME/Projects/rnastruct/data/mismatches/large/"
 
 count_mutations.py \
   -f $FA \
   -b $BAM \
-  -o $OUTDIR
+  -o $OUTDIR \
+  -t 12
   
