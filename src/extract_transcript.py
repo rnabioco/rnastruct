762b1ba5cca3413c2bf58c4986859492baf278cc
Kent Riemondy <kent.riemondy@gmail.com>
2019-06-14 16:07:05 -0600

    fix revcomp bug and use some more classes

--- src/extract_transcript.py
+++ src/extract_transcript.py
@@ -19,7 +19,7 @@ def parse_bed(fh):
         fields[2] = int(fields[2])
         yield fields
 
-revcomp = {
+bpmap = {
         "A" : "T",
         "T" : "A",
         "G" : "C",
@@ -36,7 +36,7 @@ class Mapfile:
     def __str__(self):
         return "{}\t{}\t{}".format(self.mm, self.se, self.nt)
 
-class Structpileup:
+class Structinfo:
 
     def __init__(self, string):
         vals = string.split()
@@ -56,12 +56,40 @@ class Structpileup:
                                         self.strand,
                                         self.mm,
                                         self.se)
+
+class Structpileup:
+
+    def __init__(self, string):
+        vals = string.split()
+        self.chr = vals[0]
+        self.pos = int(vals[1])
+        self.strand = vals[2]
+        self.ref_base = vals[3]
+        self.depth = int(vals[4])
+        self.refcount = int(vals[5])
+        self.acount = int(vals[6])
+        self.ccount = int(vals[7])
+        self.gcount = int(vals[8])
+        self.tcount = int(vals[9])
+        self.ncount = int(vals[10])
+        self.mmcount = int(vals[11])
+        self.delcount = int(vals[12])
+        self.inscount = int(vals[13])
+        self.mmratio = float(vals[14])
+        self.insratio = float(vals[15])
+        self.delratio = float(vals[16])
+        self.mm = float(vals[17])
+        self.se = float(vals[18])
+ 
+    def __str__(self):
+        return "\t".join([str(x) for x in vars(self).values()])
+
 def main():
     
     parser = argparse.ArgumentParser(description="""
-    Convert genomic coverage values from genomic bedgraph to .map file for Shape
-    """,
-    formatter_class = argparse.RawTextHelpFormatter )
+        Convert genomic coverage values from genomic bedgraph to .map file for Shape
+        """,
+        formatter_class = argparse.RawTextHelpFormatter )
 
     parser.add_argument('-t',
                         '--tabix-file',
@@ -121,16 +149,18 @@ def main():
                 has_strand = True
             else:
                 print("unrecognized character in column 6, expecting + or - \n ignoring strand", file = sys.stderr)
-
-        if has_strand and tx[5] == "-":
-            seqs = [revcomp[x] for x in seqs]
         
+        revcomp = False
+        if has_strand and tx[5] == "-":
+            seqs = [bpmap[x] for x in seqs]
+            revcomp = True
+
         for idx,nt in enumerate(seqs, 1):
             d[idx] = Mapfile(nt)
         
         for row in tbx.fetch(tx[0], tx[1], tx[2]):
             
-            vals = Structpileup(row)
+            vals = Structinfo(row)
 
             if has_strand and vals.strand != tx[5]:
               continue
@@ -146,8 +176,14 @@ def main():
             d[new_start].se = vals.se
             d[new_start].nt = vals.ref_base
 
-        for key,val in d.items():
-           fout.write(str(key) + "\t" + str(val) + "\n")
+        if revcomp:
+            vals = list(d.values())[::-1]
+            for idx,val in enumerate(vals):
+                nidx = idx + 1
+                fout.write(str(nidx) + "\t" + str(val) + "\n")
+        else:
+            for key,val in d.items():
+                fout.write(str(key) + "\t" + str(val) + "\n")
  
 if __name__ == '__main__': main()
 
