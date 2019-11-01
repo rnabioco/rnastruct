8a3e7a2b54f6533463bf696a5476f606d984ad44
Kent Riemondy <kent.riemondy@gmail.com>
2019-10-31 19:42:10 -0600

    more tests

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -1,4 +1,34 @@
-#! /usr/bin/env python
+#! /usr/bin/env bash 
+
+fa=$HOME/Projects/shared_dbases/genomes/human/chr_appended_grch37/Homo_sapiens.GRCh37.dna.primary_assembly_chrappended.fa 
+
+base_counter.py \
+    -b test_data/small.fwd.bam \
+    -f $fa \
+    -n 'ACTG' \
+    -t 2 \
+    -o "test_data/base_counter/fwd_" \
+    -L 'paired' \
+    -d 100 
+
+
+base_counter.py \
+    -b test_data/small.rev.bam \
+    -f $fa \
+    -n 'ACTG' \
+    -t 2 \
+    -o "test_data/base_counter/rev_" \
+    -L 'paired' \
+    -d 100 
+
+base_counter.py \
+    -b test_data/small.bam \
+    -f $fa \
+    -n 'ACTG' \
+    -t 2 \
+    -o "test_data/base_counter/both_" \
+    -L 'paired' \
+    -d 100 
 
 pytest .
 
--- src/test_rnastruct.py
+++ src/test_rnastruct.py
@@ -94,3 +94,76 @@ class TestPileuptocounts:
     assert len(chroms) == 1 and all([x in chroms for x in ["chr17"]]) 
     assert min_depth >= min_depth_arg
 
+
+class TestBaseCounter:
+    """ testing output file content for now """
+    fwd_pre = "test_data/base_counter/fwd_"
+    rev_pre = "test_data/base_counter/rev_"
+    both_pre = "test_data/base_counter/both_"
+
+    def test_both(self):
+      
+      n_lines = 0
+      strands = set()
+      chroms = set()
+      n_fields = 0
+      min_depth = 100 
+      cols = []
+      chr_strands = set()
+      uid_set = set()
+
+      with gzip.open(self.both_pre + "pileup_table.tsv.bgz", 'rt') as f:
+        for idx,line in enumerate(f):
+            fields = line.strip().split("\t")
+            if idx == 0:
+              n_fields = len(fields)
+              cols = fields
+              continue
+  
+            chrom = fields[0]
+            pos = fields[1]
+            strand = fields[2]
+            chroms.add(chrom)
+            strands.add(strand)
+            chr_strands.add((chrom, strand))
+       
+            # see if any positions are duplicated
+            if (chrom, pos, strand) in uid_set:
+                raise AssertionError("chrom, pos, and strand shouldn't be duplicated")
+            else:
+                uid_set.add((chrom, pos, strand))
+
+            depth = int(fields[4])
+            if depth < min_depth:
+                raise AttributeError("depth of {} detected".format(depth))
+
+      strands = list(strands)
+      chroms = list(chroms)
+      assert idx > 1 
+      assert len(strands) == 2 
+      assert len(chroms) == 2 and all([x in chroms for x in ["chr12", "chr17"]]) 
+      
+      assert ("chr12", "+") in chr_strands
+      assert ("chr17", "-") in chr_strands
+
+    def test_fwd(self):
+        strands = set()
+        with gzip.open(self.fwd_pre + "pileup_table.tsv.bgz", 'rt') as f:
+          for idx,line in enumerate(f):
+            fields = line.strip().split("\t")
+            if idx == 0:
+              continue  
+            strands.add(fields[2])
+
+          assert len(strands) == 1
+    
+    def test_rev(self):
+        strands = set()
+        with gzip.open(self.rev_pre + "pileup_table.tsv.bgz", 'rt') as f:
+          for idx,line in enumerate(f):
+            fields = line.strip().split("\t")
+            if idx == 0:
+              continue  
+            strands.add(fields[2])
+
+          assert len(strands) == 1
