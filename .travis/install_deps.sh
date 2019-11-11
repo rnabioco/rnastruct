c44bf2f85f3767720a25cd60f4f68180a7f3a897
Kent Riemondy <kent.riemondy@gmail.com>
2019-11-11 16:09:16 -0700

    more travis

--- .travis/install_deps.sh
+++ .travis/install_deps.sh
@@ -1,25 +1,36 @@
+#see  https://github.com/tseemann/shovill/blob/master/.travis/install_deps.sh
 
-wget https://github.com/samtools/htslib/releases/download/1.9/htslib-1.9.tar.bz2
-tar -vxjf htslib-1.9.tar.bz2
-cd htslib-1.9
-./configure prefix=/usr/local/bin
-sudo make
-sudo make install
+HERE="$PWD"
+WGET="wget --quiet"
+MAKE="make --silent -j"
+UNTAR="tar xf"
 
-cd ..
-wget https://github.com/samtools/samtools/releases/download/1.9/samtools-1.9.tar.bz2
-tar -vxjf samtools-1.9.tar.bz2
-cd samtools-1.9
-./configure prefix=/usr/local/bin
-sudo make
-sudo make install
-
-cd ..
-wget https://github.com/samtools/bcftools/releases/download/1.9/bcftools-1.9.tar.bz2
-tar -vxjf bcftools-1.9.tar.bz2
-cd bcftools-1.9
-./configure prefix=/usr/local/bin
-sudo make
-sudo make install
+HTSLIBVER=1.9
+HTSLIB=htslib-$HTSLIBVER
+echo "* $HTSLIB"
+$WGET https://github.com/samtools/htslib/releases/download/$HTSLIBVER/$HTSLIB.tar.bz2
+$UNTAR $HTSLIB.tar.bz2
+(cd $HTSLIB && ./configure --prefix=$HERE/$HTSLIB && $MAKE install)
+PATH=$HERE/$HTSLIB/bin:$PATH
 
 
+SAMTOOLSVER=1.9
+SAMTOOLS=samtools-$SAMTOOLSVER
+echo "* $SAMTOOLS"
+$WGET https://github.com/samtools/samtools/releases/download/$SAMTOOLSVER/$SAMTOOLS.tar.bz2
+$UNTAR $SAMTOOLS.tar.bz2
+(cd $SAMTOOLS && ./configure --prefix=$HERE/$SAMTOOLS && $MAKE install)
+PATH=$HERE/$SAMTOOLS/bin:$PATH
+
+BCFTOOLSVER=1.9
+BCFTOOLS=bcftools-$BCFTOOLSVER
+echo "* $BCFTOOLS"
+$WGET https://github.com/samtools/bcftools/releases/download/$BCFTOOLSVER/$BCFTOOLS.tar.bz2
+$UNTAR $BCFTOOLS.tar.bz2
+(cd $BCFTOOLS && ./configure --prefix=$HERE/$BCFTOOLS && $MAKE install)
+PATH=$HERE/$BCFTOOLS/bin:$PATH
+
+PATH=$PATH:$HERE
+
+echo $PATH
+export PATH
