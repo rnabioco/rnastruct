3aaad90107566de50fc04ab9419fd7f98cf94bd6
Kent Riemondy <kent.riemondy@gmail.com>
2020-05-11 20:00:24 -0600

    much progress, added more tests

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -5,7 +5,9 @@ l_flag='false'
 verbose='false'
 
 print_usage() {
-  printf "Usage: run_tests.sh [-c 'clean up test data files']\n"
+  printf "Usage: run_tests.sh\n"
+  printf " [-c 'clean up test data files']\n"
+  printf " [-l 'run local tests']\n"
 }
 
 while getopts 'clv' flag; do
@@ -29,36 +31,87 @@ if [[ "$l_flag" == "true" ]]; then
     echo "running local tests"
     
   fa=$HOME/Projects/shared_dbases/genomes/human/chr_appended_grch37/Homo_sapiens.GRCh37.dna.primary_assembly_chrappended.fa 
+  data_dir="test_data"
+  out_dir=$data_dir"/base_counter"
+  bams=(
+  small.fwd.bam
+  small.rev.bam
+  small.bam
+  chr16_fus_dms.bam
+  chr16_fus_ut.bam
+  )
+
+  ids=(
+  fwd_
+  rev_
+  both_
+  fus_dms_
+  fus_ut_
+  )
   
-  base_counter.py \
-      -b test_data/small.fwd.bam \
-      -f $fa \
-      -n 'ACTG' \
-      -t 2 \
-      -o "test_data/base_counter/fwd_" \
-      -L 'paired' \
-      -d 100 \
-      -v
+  vals=(
+  100
+  100
+  100
+  1
+  1
+  )
   
-  base_counter.py \
-      -b test_data/small.rev.bam \
-      -f $fa \
-      -n 'ACTG' \
-      -t 2 \
-      -o "test_data/base_counter/rev_" \
-      -L 'paired' \
-      -d 100 \
-      -v
-  
-  base_counter.py \
-      -b test_data/small.bam \
-      -f $fa \
-      -n 'ACTG' \
-      -t 2 \
-      -o "test_data/base_counter/both_" \
-      -L 'paired' \
-      -d 100 \
-      -v
+  for (( i=0; i<${#bams[@]}; i++ ))
+  do 
+    base_counter.py \
+        -b $data_dir"/"${bams[$i]} \
+        -f $fa \
+        -n 'ACTG' \
+        -t 2 \
+        -o $out_dir"/"${ids[$i]} \
+        -L 'paired' \
+        -d ${vals[$i]} \
+        -v
+  done
+
+  # filter and normalize
+  filter_and_norm.py \
+    -t $out_dir"/fus_dms_pileup_table.tsv.bgz" \
+    -u $out_dir"/fus_ut_pileup_table.tsv.bgz" \
+    -o $out_dir"/fus_norm_" \
+    -d 1 \
+    -p 2 \
+    -tr 1.0 \
+    -ur 0.02 \
+    -n 'ATCG'
+
+  # generate bedgraphs
+  to_get=(
+  5
+  11
+  12
+  15
+  17
+  19
+  21
+  )
+  cols=(
+  depth_
+  mmcount_
+  indel_count_
+  mutation_ratio_
+  mutation_ratio_ut_
+  mutation_ratio_bgsub_
+  mutation_ratio_norm_
+  )
+
+  out_bg_dir=$out_dir"/bedgraph"
+  mkdir -p $out_bg_dir
+
+  for (( i=0; i<${#to_get[@]}; i++ ))
+  do
+    tabix_to_bedgraph.py \
+        -i $out_dir"/fus_norm_pileup_table.tsv.bgz"\
+        -c ${to_get[$i]} \
+        -s "+" \
+        > $out_bg_dir"/"${cols[$i]}"pos.bedgraph"
+  done
 fi
 
 pytest .
---
+++ src/test_pileup_to_counts.py
@@ -0,0 +1,130 @@
+import gzip
+import os
+import pileup_to_counts
+
+class TestPileuptocounts:
+    
+  def test_fwd(self, tmp_path):
+    vcf = "test_data/small_fwd.vcf"
+    bam = "test_data/small.fwd.bam"
+    out = tmp_path / "counts.tsv.gz"
+    min_depth_arg = 100
+    
+    pileup_to_counts.vcf_to_counts(vcf, 
+            bam,
+            out, 
+            min_depth = min_depth_arg, 
+            return_comp = False,
+            debug = False)
+  
+    assert os.path.isfile(out)
+    
+    n_lines = 0
+    strand = set()
+    chroms = set()
+    n_fields = 0
+    min_depth = min_depth_arg
+    cols = []
+  
+    with gzip.open(out, 'rt') as f:
+      for idx,line in enumerate(f):
+          
+          n_lines += 1
+          
+          fields = line.strip().split("\t")
+          if idx == 0:
+            n_fields = len(fields)
+            cols = fields
+            continue
+  
+          chroms.add(fields[0])
+          strand.add(fields[2])
+          
+          fields[4] = int(fields[4])
+          if fields[4] < min_depth:
+              min_depth = fields[4]
+
+    strand = list(strand)
+    chroms = list(chroms)
+    assert n_lines > 1 
+    assert len(strand) == 1 and strand[0] == "+" 
+    assert len(chroms) == 1 and all([x in chroms for x in ["chr12"]]) 
+    assert min_depth >= min_depth_arg
+
+  def test_rev(self, tmp_path):
+    vcf = "test_data/small_rev.vcf"
+    bam = "test_data/small.rev.bam"
+    out = tmp_path / "counts.tsv.gz"
+    min_depth_arg = 100
+    
+    pileup_to_counts.vcf_to_counts(vcf, 
+            bam,
+            out, 
+            min_depth = min_depth_arg, 
+            return_comp = True,
+            debug = False)
+  
+    assert os.path.isfile(out)
+    
+    n_lines = 0
+    strand = set()
+    chroms = set()
+    n_fields = 0
+    min_depth = min_depth_arg
+    cols = []
+  
+    with gzip.open(out, 'rt') as f:
+      for idx,line in enumerate(f):
+          
+          n_lines += 1
+          
+          fields = line.strip().split("\t")
+          if idx == 0:
+            n_fields = len(fields)
+            cols = fields
+            continue
+  
+          chroms.add(fields[0])
+          strand.add(fields[2])
+          
+          fields[4] = int(fields[4])
+          if fields[4] < min_depth:
+              min_depth = fields[4]
+
+    strand = list(strand)
+    chroms = list(chroms)
+    assert n_lines > 1 
+    assert len(strand) == 1 and strand[0] == "-" 
+    assert len(chroms) == 1 and all([x in chroms for x in ["chr17"]]) 
+    assert min_depth >= min_depth_arg
+  
+
+  def test_indels(self, tmp_path):
+      vcf = "test_data/pileup_to_counts/fus_dms.vcf"
+      bam = "test_data/chr16_fus_dms.bam"
+      out =  tmp_path / "fus_counts.tsv.gz"
+      min_depth_arg = 1
+      expected_output = "test_data/expected_pileup_to_counts.tsv"
+
+      pileup_to_counts.vcf_to_counts(vcf, 
+            bam,
+            out, 
+            min_depth = min_depth_arg, 
+            return_comp = False,
+            debug = False)
+      
+      with open(expected_output, 'r') as exp:
+          with gzip.open(out, 'rt') as res:
+            for exp_idx, exp_line in enumerate(exp): 
+              line_found = False
+              for res_idx,res_line in enumerate(res):
+                  if exp_line.split() != res_line.split():
+                      continue
+                  else:
+                      line_found = True
+                      break
+              if not line_found:
+                  msg = "line {}, not found in output\n{}".format(exp_idx,
+                          " ".join(exp_line.split()))
+                  assert False , msg 
+              
